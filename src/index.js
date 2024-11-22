a5a7d1dce51d6b6b0f5ca11369dc235e64058c9b
Neliswa-Lab <neliswakhanyilen@gmail.com>
2024-11-22 21:26:18 +0200

    Erased the JS file and redid it from scratch

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ function updateCurrentTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-   
+
   cityElement.innerHTML = response.data.city;
-  
+
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
