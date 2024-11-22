7d4117d25892bd5302c72683b28a6af9a1067e1b
Neliswa-Lab <neliswakhanyilen@gmail.com>
2024-11-22 13:55:19 +0200

    Added new functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function updateCurrentTemperature(response){
+console.log(response.data.temperature);
+}
+
+
+function searchCity(city){
+let apiKey = "003898fo4cafab4efff03e135t07fe8f";
+let apiUrl = 
+`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateCurrentTemperature);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
