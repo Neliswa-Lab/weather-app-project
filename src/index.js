31ddcb961d8f977321f4bfd9407f657447663a97
Neliswa-Lab <neliswakhanyilen@gmail.com>
2024-11-22 15:17:22 +0200

    Updated the search function using API

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-function updateCurrentTemperature(response){
-console.log(response.data.temperature);
+function updateCurrentTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+   
+  cityElement.innerHTML = response.data.city;
+  
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
-
-function searchCity(city){
-let apiKey = "003898fo4cafab4efff03e135t07fe8f";
-let apiUrl = 
-`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(updateCurrentTemperature);
+function searchCity(city) {
+  let apiKey = "003898fo4cafab4efff03e135t07fe8f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateCurrentTemperature);
 }
-
 
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    searchCity(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
 }
-
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("Durban");
