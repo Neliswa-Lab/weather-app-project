55a067bf9ae4165d5ee64504a7369e916f5681dd
Neliswa-Lab <neliswakhanyilen@gmail.com>
2024-11-25 12:47:46 +0200

    Added the time and day

--- src/index.js
+++ src/index.js
@@ -2,10 +2,34 @@ function updateCurrentTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector("#wind-speed");
+let humidityElement = document.querySelector("#humidity");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+
+  console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
 
+  timeElement.innerHTML = formatDate(date);
+descriptionElement.innerHTML = response.data.condition.description;
+windElement.innerHTML = `${response.data.wind.speed}km/h`;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    let day = days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
