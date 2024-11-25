3a282994bfc15890f7fb90f22e32e85e44473eef
Neliswa-Lab <neliswakhanyilen@gmail.com>
2024-11-25 14:00:39 +0200

    Added weather icon

--- src/index.js
+++ src/index.js
@@ -4,32 +4,42 @@ function updateCurrentTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind-speed");
-let humidityElement = document.querySelector("#humidity");
-let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time * 1000);
+  let humidityElement = document.querySelector("#humidity");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#current-weather-icon");
 
   console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
 
   timeElement.innerHTML = formatDate(date);
-descriptionElement.innerHTML = response.data.condition.description;
-windElement.innerHTML = `${response.data.wind.speed}km/h`;
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-weather-icon"></img>`;
 }
 
-function formatDate(date){
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
-    let day = days[date.getDay()];
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    if (minutes < 10){
-        minutes = `0${minutes}`;
-    }
-    return `${day} ${hours}:${minutes}`;
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
